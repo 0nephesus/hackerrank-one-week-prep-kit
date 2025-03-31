caa4f08dcd252baf8c290ec64a1001da16eb15ed
alexmoneill97 <alexmoneill97@gmail.com>
2025-03-31 12:18:41 +0000

    feat: question template

---
+++ dist/Day-Two/Diagonal-Difference/diagonal-difference.js
@@ -0,0 +1,39 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+let inputString = '';
+let inputLines = [];
+let currentLine = 0;
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+process.stdin.on('end', function () {
+    inputLines = inputString.split('\n');
+    inputString = '';
+    main();
+});
+function readLine() {
+    return inputLines[currentLine++];
+}
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+function diagonalDifference(arr) {
+    // Write your code here
+}
+function main() {
+    const ws = (0, fs_1.createWriteStream)(process.env['OUTPUT_PATH']);
+    const n = parseInt(readLine().trim(), 10);
+    let arr = Array(n);
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+    const result = diagonalDifference(arr);
+    ws.write(result + '\n');
+    ws.end();
+}
